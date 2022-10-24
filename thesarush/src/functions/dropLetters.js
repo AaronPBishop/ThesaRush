c75bde09513222bf452bbd1d29fc00d8f54f4773
AaronPBishop <aaronpbishop@gmail.com>
2022-10-24 15:00:59 -0500

    Fixed bug causing tiles to move out of place when selected - Fixed bug preventing new tiles from spawning in columns with only one tile

--- thesarush/src/functions/dropLetters.js
+++ thesarush/src/functions/dropLetters.js
@@ -6,7 +6,7 @@ const checkEmpty = (board, randCol) => {
         if (board[randCol][i] === null) check++
     };
 
-    if (check === 10) return true;
+    if (check === 11) return true;
     return false;
 };
 
