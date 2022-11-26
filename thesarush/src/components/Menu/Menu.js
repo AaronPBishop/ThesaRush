d6d21dcefa348e331e65f6c796e62bab3f862f54
AaronPBishop <aaronpbishop@gmail.com>
2022-11-26 12:43:56 -0600

    Modified styling

--- thesarush/src/components/Menu/Menu.js
+++ thesarush/src/components/Menu/Menu.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 import { setDifficulty } from '../../store/statsReducer';
-import themeReducer, { setBackgroundColor } from '../../store/themeReducer';
+import { setBackgroundColor } from '../../store/themeReducer';
 
 import './styles.css';
 
