22dc1d1bd47540bc4e798b14e3a2d8b56673bdde
AaronPBishop <aaronpbishop@gmail.com>
2022-12-23 18:03:20 -0600

    RankedPlayer scorecards now display associated Badge components as opposed to plain-text - Enlarged OfferLife border width

--- frontend/src/components/LeaderBoard/RankedPlayer.js
+++ frontend/src/components/LeaderBoard/RankedPlayer.js
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
+import Badge from "../Badge/Badge.js";
+
+import './styles.css';
+
 const RankedPlayer = ({ score, userName, id }) => {
     const ignoreStats = ['user_id', 'user_name', 'high_score', 'points_balance', 'lives'];
+    const badges = ['bombardier', 'stone_crusher', 'gold_miner', 'word_smith', 'void_master'];
 
     const user = useSelector(state => state.user);
     const players = useSelector(state => state.players);
@@ -10,29 +15,28 @@ const RankedPlayer = ({ score, userName, id }) => {
 
     const [clicked, setClicked] = useState(false);
 
-    useEffect(() => {
-        if (clickedLeaderBoard === false) setClicked(false);
-    }, [clickedLeaderBoard])
+    useEffect(() => {if (clickedLeaderBoard === false) setClicked(false)}, [clickedLeaderBoard])
 
     const [statsMap] = useState({
         points: 'Total points Earned',
         words: 'Valid Words Submitted',
         longest_word: 'Longest Word',
-        tiles_cleared: 'Tiles Cleared',
-        bombardier: 'Bombardier Badges',
-        stone_crusher: 'Stone Crusher Badges',
-        gold_miner: 'Gold Miner Badges',
-        word_smith: 'Word Smith Badges',
-        void_master: 'Void Master Badges',
+        tiles_cleared: 'Tiles Cleared'
     });
 
+    const mapPlayerBadges = {
+        bombardier: 'bombardier',
+        stone_crusher: 'stoneCrusher',
+        gold_miner: 'goldMiner',
+        word_smith: 'wordSmith',
+        void_master: 'voidMaster'
+    };
+
     const orderKeys = (keys) => {
-        const badges = ['bombardier', 'stone_crusher', 'gold_miner', 'word_smith', 'void_master'];
         const order = [];
 
-        keys.forEach(key => {if (!badges.includes(key) && key !== 'longest_word') order.push(key)});
+        keys.forEach(key => {if (key !== 'longest_word') order.push(key)});
         order.push('longest_word');
-        keys.forEach(key => {if (badges.includes(key)) order.push(key)});
 
         return order;
     };
@@ -56,7 +60,7 @@ const RankedPlayer = ({ score, userName, id }) => {
                 cursor: 'pointer'
             }}>
                 <p style={{
-                    margin: '1vw', 
+                    margin: '1vw',
                     color: user.user_name === userName.toString() && 'rgb(255, 255, 60)',
                     fontWeight: user.user_name === userName.toString() && 'bold'
                     }}>
@@ -65,16 +69,38 @@ const RankedPlayer = ({ score, userName, id }) => {
                 <b style={{margin: '1vw'}}>{score}</b>
                 
                <div style={{display: clicked ? 'block' : 'none', marginTop: '2vh'}}>
+
+                    <div style={{display: 'flex', justifyContent: 'center', marginBottom: '1vh'}}>
+                        <div className='player-headings'>Stats</div>
+                    </div>
                     {
                         players[id] &&
                         orderKeys(Object.keys(players[id])).map((stat, i) => (
-                            !ignoreStats.includes(stat) &&
+                            (!ignoreStats.includes(stat) && !badges.includes(stat)) &&
                             <div style={{display: 'flex', justifyContent: 'space-between', width: '24vw', margin: 'auto', flexWrap: 'wrap'}} key={i}>
                                 <p style={{margin: '1vw'}}>{statsMap[stat]}:</p>
                                 <b style={{margin: '1vw'}}>{players[id][stat]}</b>
                             </div>
                         ))          
                     }
+
+                    <div style={{display: 'flex', justifyContent: 'center', marginTop: '4vh'}}>
+                        <div className='player-headings'>Badges</div>
+                    </div>
+
+                    <div style={{display: 'flex', justifyContent: 'center', flexWrap: 'wrap', marginTop: '2.5vh'}}>
+                        {
+                            players[id] &&
+                            Object.keys(players[id]).map((stat, i) => (
+                                (badges.includes(stat)) &&
+                                <div 
+                                style={{marginBottom: '1vh'}}
+                                onClick={e => e.stopPropagation()}>
+                                    <Badge badgeType={mapPlayerBadges[stat]} numBadges={players[id][stat]} key={i} />
+                                </div>
+                            ))          
+                        }
+                    </div>
                 </div>
              
             </div>
