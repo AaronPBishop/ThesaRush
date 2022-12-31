159e2635cf41e6b687bb2b2c7204119cc130b6fa
AaronPBishop <aaronpbishop@gmail.com>
2022-12-31 14:25:31 -0600

    Modified backend/app/init file

--- new_backend/app/__init__.py
+++ new_backend/app/__init__.py
@@ -2,11 +2,10 @@ from flask import Flask
 from flask_migrate import Migrate
 
 from .models import db
-
+from .seeds import seed_commands
 from .api.auth_routes import auth_routes
 from .api.user_routes import user_routes
 from .api.league_routes import league_routes
-from .api.challenge_routes import challenge_routes
 
 import os
 
@@ -17,10 +16,11 @@ app.config.from_mapping({
   'SQLALCHEMY_TRACK_MODIFICATIONS': False
 })
 
+app.cli.add_command(seed_commands)
+
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(league_routes, url_prefix='/api/leagues')
-app.register_blueprint(challenge_routes, url_prefix='/api/challenges')
 
 db.init_app(app)
 
